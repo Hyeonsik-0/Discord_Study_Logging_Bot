273023bf7b332cc2d9c789552a20161ba3c3063e
Hyeonsik Oh <oh1122@pusan.ac.kr>
2026-04-27 20:36:44 +0900

    init: spring boot project setup

---
+++ src/test/java/com/hyeonsik/studybot/StudybotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonsik.studybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
